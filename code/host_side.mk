c271749612a2eb0c4886b58ecbc93d24ad22bfd1
Josh Myer <josh@joshisanerd.com>
2021-01-30 17:22:18 -0800

    Makefiles: add docker build targets

--- code/host_side.mk
+++ code/host_side.mk
@@ -7,6 +7,18 @@
 # TODO: Implement clang-format
 # TODO: Implement unit testing on the host side
 #
+
+
+DOCKER_RUN=docker run --privileged -v $(shell pwd):/code -u $(shell id -u):$(shell id -g) -it argali
+
 indent:
 	echo clang-format -i $(PATHS)
 
+docker-image:
+	docker build -t argali -f Dockerfile .
+
+docker-build:
+	$(DOCKER_RUN) make all
+
+docker-flash:
+	$(DOCKER_RUN) make flash
