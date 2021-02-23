a569f034c3e7299861a4561bea2edba677a665de
Josh Myer <josh@joshisanerd.com>
2021-02-23 11:45:10 -0800

    console: bugfix and reformat to use macros
    The main notivation for this commit is to move over to using macros
    for all the resources used in the console code.  This is best practice
    so you don't accidentally use the wrong pin in two different places.
    
    This commit fixes a dumb buglet in the serial console code, where we
    set up pin D7 to be our RX line via AFs, but enabled pin D9 instead of
    D7.
    
    Best Practices: because you should be making *new* stupid mistakes,
    not repeating old ones!(TM)

--- code/src/console.c
+++ code/src/console.c
@@ -3,6 +3,15 @@
 /**
  * \file console.c
  * \brief Serial Console driver implementation
+ *
+ * \defgroup console Serial console
+ * \addtogroup console
+ * \{
+ *
+ * Tamodevboard supports a very basic serial console out of the box.
+ *
+ * And by "very simple," we currently mean "It prints stuff to the
+ * serial port."
  */
 
 
@@ -11,24 +20,24 @@
  */
 void console_setup(void) {
   // Enable USART3 clocks
-  rcc_periph_clock_enable(RCC_GPIOD); // USART TX pin
-  rcc_periph_clock_enable(RCC_USART3);
+  rcc_periph_clock_enable(CONSOLE_PORT_CLOCK); // USART TX pin
+  rcc_periph_clock_enable(CONSOLE_USART_CLOCK);
 
-  // Connect USART3 TX/RX pins via AF
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8);
-  gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO7);
-  gpio_set_af(GPIOD, GPIO_AF7, GPIO8 | GPIO9);
+  // Connect CONSOLE_USART TX/RX pins via AF
+  gpio_mode_setup(CONSOLE_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CONSOLE_TX_PIN);
+  gpio_mode_setup(CONSOLE_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, CONSOLE_RX_PIN);
+  gpio_set_af(CONSOLE_PORT, CONSOLE_AFNO, CONSOLE_RX_PIN | CONSOLE_TX_PIN);
 
   // Now actually set up the USART peripheral
-  usart_set_baudrate(USART3, 115200);
-  usart_set_databits(USART3, 8);
-  usart_set_stopbits(USART3, USART_STOPBITS_1);
-  usart_set_mode(USART3, USART_MODE_TX_RX);
-  usart_set_parity(USART3, USART_PARITY_NONE);
-  usart_set_flow_control(USART3, USART_FLOWCONTROL_NONE);
+  usart_set_baudrate(CONSOLE_USART, CONSOLE_BAUD);
+  usart_set_databits(CONSOLE_USART, 8); // We'll leave the 8N1 hard-coded here for now...
+  usart_set_stopbits(CONSOLE_USART, USART_STOPBITS_1);
+  usart_set_mode(CONSOLE_USART, USART_MODE_TX_RX);
+  usart_set_parity(CONSOLE_USART, USART_PARITY_NONE);
+  usart_set_flow_control(CONSOLE_USART, USART_FLOWCONTROL_NONE);
 
   // And ... engage!
-  usart_enable(USART3);
+  usart_enable(CONSOLE_USART);
 }
 
 /**
@@ -36,5 +45,7 @@ void console_setup(void) {
  */
 void console_send_blocking(const char c)
 {
-  usart_send_blocking(USART_CONSOLE, c);
+  usart_send_blocking(CONSOLE_USART, c);
 }
+
+/** \} */ // End doxygen group
--- code/src/console.h
+++ code/src/console.h
@@ -3,6 +3,9 @@
 /**
  * \file console.h
  * \brief Header file for console interface
+ *
+ * \addtogroup console
+ * \{
  */
 
 #include <libopencm3/stm32/gpio.h>
@@ -10,7 +13,19 @@
 
 #include <libopencm3/stm32/usart.h>
 
-#define USART_CONSOLE USART3
+#define CONSOLE_USART USART3 //!< Which USART peripheral to use for console output
+#define CONSOLE_USART_CLOCK RCC_USART3 //!< Clock for the USART peripheral itself
+
+#define CONSOLE_PORT_CLOCK RCC_GPIOD //!< Which clock to enable for the console rx/tx pins
+
+#define CONSOLE_PORT GPIOD //!< The GPIO Port used for the console's data pins
+#define CONSOLE_TX_PIN GPIO8 //!< The GPIO pin used for serial TX
+#define CONSOLE_RX_PIN GPIO9 //!< The GPIO pin used for serial RX
+#define CONSOLE_AFNO GPIO_AF7 //!< Which alternate function to use for this port
+
+#define CONSOLE_BAUD 115200 //!< Baud rate of the serial console
 
 void console_setup(void);
 void console_send_blocking(const char);
+
+/** \} */ // End doxygen group
