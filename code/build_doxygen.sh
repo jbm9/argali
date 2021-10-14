77978094b54c93e43dbe558a58ac2abd8e1af803
Josh Myer <josh@joshisanerd.com>
2021-10-14 15:57:38 -0700

    build_doxygen.sh: use the new version script
    Now that the version number extraction has been pulled out into a
    standalone shell script, we use it in the doxygen build process.

--- code/build_doxygen.sh
+++ code/build_doxygen.sh
@@ -7,33 +7,7 @@
 # thing.
 #
 
-# Also note that we need to have a janky multi-mount, as /code doesn't
-# contain its own .git checkout.  This means that the git metadata
-# isn't available to us without multiple `-v` arguments to the docker
-# run command.
-GIT="git -C /project_base"
-
-version=$(cat VERSION)
-
-gitrev=$(${GIT} rev-parse --short HEAD)
-
-# Check if git is dirty by looking at files known to git.
-#
-# A quick note on why it's sufficient to only check for changes in
-# known files: to compile new files into the final binary, they have
-# to be added to the Makefile, or included in the source code, or
-# similar.  In general, it's not possible to add a new file to a build
-# without also touching existing files.  Thus, it's possible to get a
-# clean build if a file isn't checked in, the CI tests should catch
-# that, as things should fail to compile in that environment.
-
-${GIT} diff-files --quiet
-if [ "x$?" = "x1" ];
-then
-    gitrev="${gitrev}-dirty"
-fi
-
-full_version="${version}-${gitrev}"
+full_version=$(./argali_version.sh)
 
 [ $DEBUG ] && echo "Building doxygen for ${full_version}"
 
